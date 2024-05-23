9353bd1baeeaa536c12244762812c93b9af4b40d
cedced1551 <ces0105@naver.com>
2024-05-23 17:28:05 +0900

    Add files via upload
    echo_epollserv.c 는 echo_client.c로 동작 나머지는 echo_client2.c 로 동작

--- echo_client2.c
+++ echo_client2.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int sock;
 	char message[BUF_SIZE];
-	int str_len;
+	int str_len, recv_len, recv_cnt;
 	struct sockaddr_in serv_adr;
 
 	if(argc!=3) {
@@ -38,22 +38,23 @@ int main(int argc, char *argv[])
 	{
 		fputs("Input message(Q to quit): ", stdout);
 		fgets(message, BUF_SIZE, stdin);
-
-		write(sock, message, strlen(message));
-		str_len=read(sock, message, BUF_SIZE-1);
-		message[str_len]=0;
 		
-		if (str_len <= 0)
+		if(!strcmp(message,"q\n") || !strcmp(message,"Q\n"))
 			break;
 
-		if (!strcmp(message, "End of Message\n")){
-
-			printf("Message from server: %s", message);
-			break;
+		str_len=write(sock, message, strlen(message));
+		
+		recv_len=0;
+		while(recv_len<str_len)
+		{
+			recv_cnt=read(sock, &message[recv_len], BUF_SIZE-1);
+			if(recv_cnt==-1)
+				error_handling("read() error!");
+			recv_len+=recv_cnt;
 		}
-
+		
+		message[recv_len]=0;
 		printf("Message from server: %s", message);
-
 	}
 	
 	close(sock);
